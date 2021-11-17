f3b5f712cab87e397b029d30677d66d24d6ae155
lordsteva <lordsteva@hotmail.com>
2021-11-17 23:17:58 +0100

    fix example

--- apps/psp-front/src/Component.tsx
+++ apps/psp-front/src/Component.tsx
@@ -1,21 +1,19 @@
+import { Button } from '@team21/ui-components';
 import React, { FC } from 'react';
 import useGetUsers from './graphql/useGetUsers';
 
-type Props = {
-  title?: string;
-  onClick?: React.MouseEventHandler<HTMLButtonElement>;
-};
+const Component: FC<Record<string, never>> = () => {
+  const { data, loading, error } = useGetUsers();
+  if (loading) return <div>loading....</div>;
+  if (error) return <div>error</div>;
 
-const Component: FC<Props> = ({ onClick, title }) => {
-  const { data, loading } = useGetUsers();
-  if (loading) return <div>xd</div>;
   return (
-    <button onClick={onClick} className={'bg-red-800 border-gray-800 border'}>
-      {title && <span className="w-full p-3">{title}</span>}
+    <div>
+      <Button onClick={() => console.log('clic;')} title="btn" />
       {data.users.map((u: any) => (
         <div key={u}>{u.username}</div>
       ))}
-    </button>
+    </div>
   );
 };
 
