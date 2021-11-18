6a31df29f44de55e6314767c2200c1d30f1be0bf
lordsteva <lordsteva@hotmail.com>
2021-11-18 19:33:16 +0100

    remove hasura example

--- apps/psp-back/hasura/migrations/default/1637095246377_create_table_public_user/up.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE "public"."user" ("username" text NOT NULL, "id" uuid NOT NULL DEFAULT gen_random_uuid(), PRIMARY KEY ("id") , UNIQUE ("username"));
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
