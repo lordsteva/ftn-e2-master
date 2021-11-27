93ce6455d28490ffa7b0caaf13484c69e6ddfb81
jelenabudisa <58332793+jelenabudisa@users.noreply.github.com>
2021-11-27 22:37:14 +0100

    fix-front

--- apps/psp-front/src/Component.tsx
+++ apps/psp-front/src/Component.tsx
@@ -1,22 +1,64 @@
-import React, { FC, useState } from 'react';
-import useGetUsers from './graphql/useGetUsers';
-import { Button, Input, Select, Checkbox, TextArea, Search, Image, Loader, Card} from '@team21/ui-components/'
+import {
+  Button,
+  Card,
+  Checkbox,
+  Image,
+  Loader,
+  Search,
+  Select,
+  TextArea,
+} from '@team21/ui-components/';
+import React, { FC } from 'react';
 
 const Component: FC<Record<string, never>> = () => {
-  return <div>
-    <Loader />
-    <Card title="Card Title" body={<li> <b> AA </b> </li>} buttonTitle="Submit" imageSrc="https://mdbootstrap.com/img/new/standard/city/031.jpg"/>
-    <br/>
-    <Button disabled={true} buttonColor="primary" textColor="whitesmoke" onClick={()=>{console.log('AAA')}} title="button" />
-    <Button disabled={true} buttonColor="secondary" textColor="whitesmoke" onClick={()=>{console.log('AAA')}} title="button" />
+  return (
+    <div>
+      <Loader />
+      <Card
+        title="Card Title"
+        body={
+          <li>
+            {' '}
+            <b> AA </b>{' '}
+          </li>
+        }
+        buttonTitle="Submit"
+        imageSrc="https://mdbootstrap.com/img/new/standard/city/031.jpg"
+      />
+      <br />
+      <Button
+        disabled={true}
+        buttonColor="primary"
+        textColor="whitesmoke"
+        onClick={() => {
+          console.log('AAA');
+        }}
+        title="button"
+      />
+      <Button
+        disabled={true}
+        buttonColor="secondary"
+        textColor="whitesmoke"
+        onClick={() => {
+          console.log('AAA');
+        }}
+        title="button"
+      />
 
-    <Input errorText="This is a error text ajajjajajaajaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" id="input" type="text" labelText="FirstName"/>
-    <Select options={[{text:'1', value:'1'}, {text:'2', value:'2'}]} id="select" labelText="Select Something" />
-    <Checkbox id='checkbox' labelText="You agree?" />
-    <TextArea id="textarea" />
-    <Search id="search" />
-    <Image  width='1/12' src="a" />
-  </div>;
+      <Select
+        options={[
+          { text: '1', value: '1' },
+          { text: '2', value: '2' },
+        ]}
+        id="select"
+        labelText="Select Something"
+      />
+      <Checkbox id="checkbox" labelText="You agree?" />
+      <TextArea id="textarea" />
+      <Search id="search" />
+      <Image width="1/12" src="a" />
+    </div>
+  );
 };
 
 export default Component;
--- packages/ui-components/src/input-controls/Input/Input.tsx
+++ packages/ui-components/src/input-controls/Input/Input.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import { UseFormRegisterReturn } from 'react-hook-form';
+import type { UseFormRegisterReturn } from 'react-hook-form';
 
 type Props = {
   id: string;
