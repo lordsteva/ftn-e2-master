ea181e53ea0f56fd58ccff576410f6fb31eb7170
jelenabudisa <58332793+jelenabudisa@users.noreply.github.com>
2021-11-27 22:37:58 +0100

    feat-web-shop-login-front

--- apps/web-shop-front/src/pages/LoginPage.tsx
+++ apps/web-shop-front/src/pages/LoginPage.tsx
@@ -1,10 +1,18 @@
 import { Login } from '@team21/ui-components';
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
+import useLogin from '../graphql/useLogin';
 
 const LoginPage: FC<Record<string, never>> = () => {
+  const [sendLoginRequest, { data, loading, error, called }] = useLogin();
+  useEffect(() => {
+    if (!called) return;
+    if (data) {
+      console.log(data);
+    }
+  }, [data, called]);
   return (
     <div className="flex flex-col items-center justify-between w-full h-screen ">
-      <Login onClick={(u, p) => console.log('successssss')} />
+      <Login onClick={(u, p) => sendLoginRequest({ variables: { username: u, password: p } })} />
     </div>
   );
 };
