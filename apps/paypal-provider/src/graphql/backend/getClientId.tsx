6c89e6385693c17b63888c8404725ff2d2cf425a
Stevan Rašković <lordsteva@hotmail.com>
2021-11-30 23:54:26 +0100

    add: ggl queries and mutations

--- apps/paypal-provider/src/graphql/backend/getClientId.tsx
+++ apps/paypal-provider/src/graphql/backend/getClientId.tsx
@@ -5,6 +5,8 @@ const query = gql`
   query GetClientId($id: uuid!) {
     payment_intents_by_pk(id: $id) {
       api_key
+      success_url
+      fail_url
       apiKeyByApiKey {
         api_provider_links {
           metadata
@@ -16,11 +18,16 @@ const query = gql`
 
 const getClientId = async (variables: {
   id: string;
-}): Promise<{ apiKey: string | undefined; metadata: string | undefined }> => {
+}): Promise<{
+  apiKey: string | undefined;
+  metadata: string | undefined;
+  success_url: string | undefined;
+  fail_url: string | undefined;
+}> => {
   const res = await graphqlAdminClient.query({ query, variables });
-  const apiKey = res.data.payment_intents_by_pk?.api_key;
+  const { api_key, success_url, fail_url } = res.data.payment_intents_by_pk;
   const metadata = res.data.payment_intents_by_pk?.apiKeyByApiKey?.api_provider_links?.[0].metadata;
-  return { apiKey, metadata };
+  return { apiKey: api_key, metadata, success_url, fail_url };
 };
 
 export default getClientId;
---
+++ apps/paypal-provider/src/graphql/backend/insertOrder.tsx
@@ -0,0 +1,40 @@
+import { gql } from '@apollo/client';
+import graphqlAdminClient from './admin-client';
+
+const mutation = gql`
+  mutation InsertOrder(
+    $state: String!
+    $payment_provider_id: uuid!
+    $payment_intent_id: uuid!
+    $metadata: String!
+    $external_id: String!
+  ) {
+    insert_orders_one(
+      object: {
+        external_id: $external_id
+        metadata: $metadata
+        payment_intent_id: $payment_intent_id
+        payment_provider_id: $payment_provider_id
+        state: $state
+      }
+    ) {
+      id
+    }
+  }
+`;
+
+const insertOrder = async (variables: {
+  state: string;
+  payment_provider_id: string;
+  payment_intent_id: string;
+  metadata: string;
+  external_id: string;
+}): Promise<{
+  id: string | undefined;
+}> => {
+  const res = await graphqlAdminClient.mutate({ mutation, variables });
+  const { id } = res?.data?.insert_orders_one;
+  return { id };
+};
+
+export default insertOrder;
