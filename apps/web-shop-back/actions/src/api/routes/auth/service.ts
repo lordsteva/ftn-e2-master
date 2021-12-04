8fbd9df45e374851766111288d9ec72174382051
Stevan Rašković <lordsteva@hotmail.com>
2021-12-04 11:16:53 +0100

    fix: replace bcrypt with bcryptjs since first one can't run on M1

--- apps/web-shop-back/actions/src/api/routes/auth/service.ts
+++ apps/web-shop-back/actions/src/api/routes/auth/service.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import config from '../../../config/constants';
