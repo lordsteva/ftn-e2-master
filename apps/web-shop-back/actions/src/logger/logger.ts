13743896b410a70fc12c31b7652edee450279848
jelenabudisa <58332793+jelenabudisa@users.noreply.github.com>
2021-12-06 23:45:49 +0100

    fix-logging

--- apps/web-shop-back/actions/src/logger/logger.ts
+++ apps/web-shop-back/actions/src/logger/logger.ts
@@ -27,19 +27,41 @@ const format = logform.combine(
 );
 
 const transports = [
-  new winston.transports.Console(),
+  new winston.transports.Console({
+    format: logform.combine(
+      logform.timestamp(),
+      logform.colorize({ all: true }),
+      logform.printf(
+        (information) => `${information.timestamp} ${information.level}: ${information.message}`,
+      ),
+    ),
+  }),
   new winston.transports.File({
     filename: 'logs/error.log',
     level: 'error',
+    format: logform.combine(
+      logform.timestamp(),
+      logform.printf(
+        (information) => `${information.timestamp} ${information.level}: ${information.message}`,
+      ),
+    ),
     maxsize: 5242880,
   }),
-  new winston.transports.File({ filename: 'logs/all.log', maxsize: 5242880 }),
+  new winston.transports.File({
+    filename: 'logs/all.log',
+    format: logform.combine(
+      logform.timestamp(),
+      logform.printf(
+        (information) => `${information.timestamp} ${information.level}: ${information.message}`,
+      ),
+    ),
+    maxsize: 5242880,
+  }),
 ];
 
 const logger = winston.createLogger({
   level: 'debug',
   levels,
-  format,
   transports,
 });
 
