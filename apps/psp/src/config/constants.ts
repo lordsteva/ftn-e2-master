e1edf8d83b73b8b1c85417a98fdd05ea50e3f99e
Stevan Rašković <lordsteva@hotmail.com>
2021-12-08 22:31:49 +0100

    migrate react router to nextjs routing

---
+++ apps/psp/src/config/constants.ts
@@ -0,0 +1,8 @@
+const config = {
+  PORT: process.env.PORT || 8080,
+  HASURA_ADMIN_SECRET_HEADER_NAME: process.env.HASURA_ADMIN_SECRET_HEADER_NAME,
+  HGE_ADMIN_SECRET: process.env.HGE_ADMIN_SECRET,
+  HGE_ENDPOINT: process.env.NEXT_PUBLIC_HGE_ENDPOINT,
+};
+
+export default config;
---
+++ apps/psp/src/pages/api/transaction/link.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from 'express';
+import { v4 as uuidv4 } from 'uuid';
+import getApiKeyData from '../../../graphql/backend/getApiKeyData';
+import insertPaymentIntent from '../../../graphql/backend/insertPaymentIntent';
+import logger from '../../../logger/logger';
+
+export default async (req: Request, res: Response): Promise<Response> => {
+  const { data } = req.body.input;
+  const { api_key, api_secret, amount, currency, success_url, fail_url } = data;
+
+  logger.info(`Creating payment intent with api key:  ${api_key}`);
+  const apiKeyData = await getApiKeyData(api_key);
+
+  if (!apiKeyData || !apiKeyData.active || api_secret !== apiKeyData.api_secret) {
+    logger.error(`Invalid api key:  ${api_key}`);
+    return res.status(400).json({
+      message: 'Invalid API key or secret',
+    });
+  }
+  const transactionData = {
+    api_key,
+    amount,
+    currency,
+    id: uuidv4(),
+    created_at: new Date().toISOString(),
+    success_url,
+    fail_url,
+  };
+
+  await insertPaymentIntent(transactionData);
+  logger.info(`Succesfully created payment intent ${transactionData.id} with api key:  ${api_key}`);
+  return res.json({
+    link: transactionData.id,
+  });
+};
