a393e02e118e332f8fde7feb7a02d970e47e6c26
Stefan <s.jokic@beeit.rs>
2022-01-28 17:26:04 +0100

    update

--- apps/web-shop-front/src/pages/Orders.tsx
+++ apps/web-shop-front/src/pages/Orders.tsx
@@ -1,4 +1,4 @@
-import { Loader, Pagination, Button } from '@team21/ui-components';
+import { Loader, Pagination, Button, Image } from '@team21/ui-components';
 import React, { FC, useState, useCallback } from 'react';
 import useGetUserOrders from '../graphql/order/useGetUserOrders';
 import { useUser } from '@team21/web-shop-front/src/state/state';
@@ -8,7 +8,18 @@ const PER_PAGE = 6;
 const Category: FC<Record<string, never>> = () => {
     const [{ user }] = useUser();
     const [currentPage, setCurrentPage] = useState(0);
-    const [viewOrder, setViewOrder] = useState(false);
+    const [viewMore, setViewMore] = useState(false);
+    const [orderView, setOrderView] = useState({
+        id: '',
+        created_at: '',
+        status: '',
+        total_price: '',
+        address: '',
+        city: '',
+        country: '',
+        phone: '',
+        orderProducts: []
+    });
     const { data, loading } = useGetUserOrders(user.id, PER_PAGE, currentPage * PER_PAGE);
 
     const onPageChange = useCallback(
@@ -25,6 +36,11 @@ const Category: FC<Record<string, never>> = () => {
         return dat[2]+"/"+dat[1]+"/"+dat[0]+" "+time[0]
     }
 
+    function viewOrder (order: any) {
+        setViewMore(true)
+        setOrderView(order)
+    }
+
     if(!data && loading) return <Loader />;
 
     const total = data?.orders_aggregate.aggregate.count
@@ -34,7 +50,69 @@ const Category: FC<Record<string, never>> = () => {
         <React.Fragment>
             <div className="w-10/12 p-24 mx-auto">
                 {
-                viewOrder ? <div> a </div> : <React.Fragment>
+                viewMore ? 
+                <div className='mx-auto'>
+                    <div className='flex justify-between items-center border-b-default border-solid border-lightGray mb-16'>
+                        <span className='text-whitesmoke text-xxl'>Order Information</span>
+                        <Button
+                            buttonColor="whitesmoke"
+                            textColor="black"
+                            rounded={true}
+                            onClick={()=>setViewMore(false)}
+                            title="Back to Orders"
+                            customClass='mb-4'
+                        />
+                    </div>
+                    <div className='flex text-whitesmoke text-xl my-8'>
+                        <span className='w-1/4'> Order ID </span>
+                        <span className='w-1/4'> Order Date </span>
+                        <span className='w-1/4'> Order Status </span>
+                        <span className='w-1/4'> Order Total </span>
+                    </div>
+                    <div className='flex text-whitesmoke mb-24'>
+                        <span className='w-1/4'>{orderView.id}</span>
+                        <span className='w-1/4'> {convertDate(orderView.created_at)} </span>
+                        <span className='w-1/4'> {orderView.status} </span>
+                        <span className='w-1/4'> ${orderView.total_price} </span>
+                    </div>
+                    <div className='flex text-whitesmoke text-xl my-8'>
+                        <span className='w-1/4'> Country </span>
+                        <span className='w-1/4'> City </span>
+                        <span className='w-1/4'> Address </span>
+                        <span className='w-1/4'> Phone </span>
+                    </div>
+                    <div className='flex text-whitesmoke mb-24'>
+                        <span className='w-1/4'>{orderView.country}</span>
+                        <span className='w-1/4'> {orderView.city} </span>
+                        <span className='w-1/4'> {orderView.address} </span>
+                        <span className='w-1/4'> ${orderView.phone} </span>
+                    </div>
+                    <div>
+                        <ul role="list"> 
+                        { 
+                            orderView.orderProducts.map((item:any) => (
+                                <li key={item.product.id} className="flex mb-16 h-180px">
+                                <Image
+                                    src={item.product.image}
+                                    alt={`${item.product.name}-image`}
+                                    wrapperClassName="flex justify-center items-center w-1/5 border-default border-solid border-lightGray"
+                                />
+                                <div className="flex flex-col flex-1 pb-12 mx-24">
+                                    <div className="flex justify-between text-xl font-medium text-whitesmoke">
+                                    <h3>{item.product.name}</h3>
+                                    <p className="ml-4">${item.product.price}</p>
+                                    </div>
+                                    <div className="flex items-end justify-between flex-1 text-md">
+                                    <p className="text-lightGray">Qty: {item.product.quantity}</p>
+                                    </div>
+                                </div>
+                            </li>
+                            ))
+                        }               
+                        </ul>
+                    </div>
+                </div> : 
+                <React.Fragment>
                     <h1 className="text-whitesmoke text-h1 text-center mb-24 ml-24">Orders</h1>
                     <div className="flex justify-items-start items-center flex-wrap">
                         {data?.orders?.map((order: any)=>(
@@ -58,7 +136,7 @@ const Category: FC<Record<string, never>> = () => {
                                         buttonColor="whitesmoke"
                                         textColor="black"
                                         rounded={true}
-                                        onClick={undefined}
+                                        onClick={()=>viewOrder(order)}
                                         title="View Order"
                                         customClass='mt-16'
                                     />
