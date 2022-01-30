082433958ce0e97dcd7599864570dbac9e7c4368
Stevan Rašković <lordsteva@hotmail.com>
2022-01-30 14:53:55 +0100

    web shop updates

--- packages/ui-components/src/input-controls/Select/Select.tsx
+++ packages/ui-components/src/input-controls/Select/Select.tsx
@@ -1,9 +1,10 @@
 import React, { FC } from 'react';
+import type { UseFormRegisterReturn } from 'react-hook-form';
 
 type Option = {
-    value: any;
-    text: string;
-}
+  value: any;
+  text: string;
+};
 
 type Props = {
   id: string;
@@ -13,22 +14,44 @@ type Props = {
   errorText?: string;
   multiple?: boolean;
   options: Option[];
+  // default selection
+  defaultValue?: any;
+  innerRef?: UseFormRegisterReturn;
 };
 
-const Select: FC<Props> = ({ id, name, wrapperClassName, labelText, errorText, options, multiple }) => (
-    <div className={wrapperClassName}>
-        <label htmlFor={id}>
-           {labelText && <span>{labelText}</span>}
-        </label>
-        <div>
-            <select multiple={multiple} id={id} name={name} className={"w-full cursor-pointer bg-white text-dark border-default border-solid border-lightGray hover:border-gray focus:outline-none focus:border-dark pl-4 my-8"}>
-                {options.map((option: Option, index: number) => (
-                    <option key={index} value={option.value}>{option.text}</option>
-                ))}
-            </select>
-        </div>
-        {errorText && <p className='text-sm mb-2 text-red'>{errorText}</p>}
+const Select: FC<Props> = ({
+  id,
+  name,
+  innerRef,
+  wrapperClassName,
+  labelText,
+  errorText,
+  options,
+  multiple,
+  defaultValue,
+}) => (
+  <div className={wrapperClassName}>
+    <label htmlFor={id}>{labelText && <span>{labelText}</span>}</label>
+    <div>
+      <select
+        defaultValue={defaultValue}
+        multiple={multiple}
+        id={id}
+        name={name}
+        className={
+          'w-full cursor-pointer bg-white text-dark border-default border-solid border-lightGray hover:border-gray focus:outline-none focus:border-dark pl-4 my-8'
+        }
+        {...innerRef}
+      >
+        {options.map((option: Option, index: number) => (
+          <option key={index} value={option.value}>
+            {option.text}
+          </option>
+        ))}
+      </select>
     </div>
+    {errorText && <p className="mb-2 text-sm text-red">{errorText}</p>}
+  </div>
 );
 
 export default Select;
