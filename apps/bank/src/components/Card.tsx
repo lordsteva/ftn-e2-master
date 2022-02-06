b76fb1b3d5fe5e7d3dd92192914c2469b86545aa
jelenabudisa <58332793+jelenabudisa@users.noreply.github.com>
2022-02-06 21:51:22 +0100

    fix-conflict-resolved

--- apps/bank/src/components/Card.tsx
+++ apps/bank/src/components/Card.tsx
@@ -43,54 +43,66 @@ const Card: FC<Props> = ({
   return (
     <div className="max-w-xl px-4 py-4 m-auto mt-64 text-lg text-center border-2 border-gray-900">
       <form onSubmit={handleSubmit(onSubmit)} className="p-16">
-        <div className="pb-8 text-4xl font-bold text-center text-blue-800 text-h2">Pay</div>
-        amount to pay: {amount}
-        <Input
-          id="pan"
-          labelText="Pan"
-          type="text"
-          placeholder="Please enter PAN"
-          innerRef={register('pan', {
-            required: 'PAN required',
-          })}
-          errorText={errors.pan?.message}
-        />
-        <div className="py-8" />
-        <Input
-          id="ccv"
-          labelText="CCV"
-          type="text"
-          placeholder="Please enter CCV"
-          innerRef={register('ccv', {
-            required: 'CCV required',
-          })}
-          errorText={errors.ccv?.message}
-        />
-        <div className="py-8" />
-        <Input
-          id="expire"
-          labelText="Expiration"
-          type="text"
-          placeholder="Please enter expiration date"
-          innerRef={register('expire', {
-            required: 'Expiration date required',
-          })}
-          errorText={errors.expire?.message}
-        />
-        <div className="py-8" />
-        <Input
-          id="holder"
-          labelText="Card holder"
-          type="text"
-          placeholder="Please enter card holder name"
-          innerRef={register('holder', {
-            required: 'Card holder required',
-          })}
-          errorText={errors.holder?.message}
-        />
+        <div className="pb-8 mb-16 font-bold text-center text-xxl text-whitesmoke border-b-default border-gray">
+          Pay
+        </div>
+        <div className="mb-16 text-left text-whitesmoke"> Amount to pay: {amount} </div>
+        <div className="text-left">
+          <Input
+            id="pan"
+            labelText="Pan"
+            type="text"
+            placeholder="Please enter PAN"
+            innerRef={register('pan', {
+              required: 'PAN required',
+            })}
+            errorText={errors.pan?.message}
+            customClass="mb-16 rounded-xs bg-white text-dark"
+            labelClass="text-whitesmoke text-lg"
+          />
+          <div className="py-8" />
+          <Input
+            id="ccv"
+            labelText="CCV"
+            type="text"
+            placeholder="Please enter CCV"
+            innerRef={register('ccv', {
+              required: 'CCV required',
+            })}
+            errorText={errors.ccv?.message}
+            customClass="mb-16 rounded-xs bg-white text-dark"
+            labelClass="text-whitesmoke text-lg"
+          />
+          <div className="py-8" />
+          <Input
+            id="expire"
+            labelText="Expiration"
+            type="text"
+            placeholder="Please enter expiration date"
+            innerRef={register('expire', {
+              required: 'Expiration date required',
+            })}
+            errorText={errors.expire?.message}
+            customClass="mb-16 rounded-xs bg-white text-dark"
+            labelClass="text-whitesmoke text-lg"
+          />
+          <div className="py-8" />
+          <Input
+            id="holder"
+            labelText="Card holder"
+            type="text"
+            placeholder="Please enter card holder name"
+            innerRef={register('holder', {
+              required: 'Card holder required',
+            })}
+            errorText={errors.holder?.message}
+            customClass="mb-16 rounded-xs bg-white text-dark"
+            labelClass="text-whitesmoke text-lg"
+          />
+        </div>
         <div className="py-8" />
         <div className="flex justify-center pt-12 pb-4">
-          <Button size="90" title="PAY" disabled={isSubmitting}></Button>
+          <Button size="90" title="PAY" disabled={isSubmitting} block></Button>
         </div>
       </form>
     </div>
